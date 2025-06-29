7a4f7db1d5d3456ebcb1f5d3dff014d2806b3d52
Thomas McBoyle <tom.mcboyle@gmail.com>
2025-06-29 10:20:54 +0100

    Add watermark support to ingestion.py

--- core/source/modules/petl/ingestion.py
+++ core/source/modules/petl/ingestion.py
@@ -1,6 +1,11 @@
 from pyspark.sql import DataFrame, SparkSession
+from typing import Any, Optional
 
-def read_volume_csv(spark: SparkSession, path: str) -> DataFrame:
+def read_volume_csv(spark: SparkSession, path: str, watermarkColumn: Optional[str]=None, watermarkValue: Optional[Any]=None) -> DataFrame:
     """ Read data from an xlsx file into a dataframe """
     df = spark.read.format("csv").load(path)
+
+    if watermarkColumn is not None and watermarkValue is not None:
+        df = df.filter(df['watermarkColumn'] > watermarkValue)
+
     return df
