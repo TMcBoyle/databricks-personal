41e6b330c3fba023a19ead18e03bcfde88c233dd
TMcBoyle <TMcBoyle>
2025-06-29 18:15:09 +0000

    Ingestion setup

--- core/source/modules/petl/ingestion.py
+++ core/source/modules/petl/ingestion.py
@@ -3,9 +3,9 @@ from typing import Any, Optional
 
 def read_volume_csv(spark: SparkSession, path: str, watermarkColumn: Optional[str]=None, watermarkValue: Optional[Any]=None) -> DataFrame:
     """ Read data from an xlsx file into a dataframe """
-    df = spark.read.format("csv").load(path)
+    df = spark.read.format("csv").option("header", True).load(path)
 
     if watermarkColumn is not None and watermarkValue is not None:
-        df = df.filter(df['watermarkColumn'] > watermarkValue)
+        df = df.filter(df[watermarkColumn] > watermarkValue)
 
     return df
--- core/source/notebooks/Ingest COINS.py
+++ core/source/notebooks/Ingest COINS.py
@@ -1,4 +1,57 @@
 import os, sys
 sys.path.append(os.path.abspath("../modules/"))
 
+from petl.ingestion import read_volume_csv
 
+config = [
+    {
+        "entity": "coins_company",
+        "source_system": "coins",
+        "source_type": "volume_csv",
+        "connection_details": {
+            "volume_path": "/Volumes/source/data/files/COINS_company.csv"
+        },
+        "watermark": "jtlastupdate"
+    },
+    {
+        "entity": "coins_development",
+        "source_system": "coins",
+        "source_type": "volume_csv",
+        "connection_details": {
+            "volume_path": "/Volumes/source/data/files/COINS_development.csv"
+        },
+        "watermark": "jtlastupdate"
+    },
+    {
+        "entity": "coins_plot",
+        "source_system": "coins",
+        "source_type": "volume_csv",
+        "connection_details": {
+            "volume_path": "/Volumes/source/data/files/COINS_plot.csv"
+        },
+        "watermark": "jtlastupdate"
+    },
+    {
+        "entity": "coins_sales",
+        "source_system": "coins",
+        "source_type": "volume_csv",
+        "connection_details": {
+            "volume_path": "/Volumes/source/data/files/COINS_sales.csv"
+        },
+        "watermark": "jtlastupdate"
+    }
+]
+
+for entity in config:
+    if entity['source_type'] == 'volume_csv':
+        max_watermark = None
+        if entity['watermark'] != "null":
+            try:
+                max_watermark = spark.sql(f"SELECT MAX({entity['watermark']}) FROM bronze.{entity['source_system']}.{entity['entity']}").collect()[0][0]
+            except:
+                pass
+
+        df = read_volume_csv(spark, entity['connection_details']['volume_path'], entity['watermark'], max_watermark)
+
+    display(df)
+    #df.write.mode("append").option("mergeSchema", True).saveAsTable(f"bronze.{entity['source_system']}.{entity['entity']}")
